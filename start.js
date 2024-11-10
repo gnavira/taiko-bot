6de3567d7cac1eb5f71c8ca3c23bdcf818f991e5
Navira Gayatri <141285340+gnavira@users.noreply.github.com>
2024-11-11 03:41:03 +0700

    Update start.js

--- start.js
+++ start.js
@@ -312,13 +312,4 @@ const job = new CronJob(
   true,
   'UTC'
 );
-runWrapandUnwrap()
-  .then(() => {
-    console.log('First run of runWrapandUnwrap completed.');
-    job.start();
-    console.log('Transaction will run every 01:00 UTC');
-  })
-  .catch(error => {
-    console.error(`Error during the first run: ${error.message}`);
-  });
 
