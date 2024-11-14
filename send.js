b349a9526d6d520523b61c55f8d33f87f4f76075
Navira Gayatri <141285340+gnavira@users.noreply.github.com>
2024-11-14 09:55:56 +0700

    Update send.js

--- send.js
+++ send.js
@@ -1,35 +1,79 @@
 const { ethers } = require('ethers');
 const chains = require('./chains');
 const provider = chains.mainnet.taiko.provider;
+const changeProvider = chains.mainnet.taiko.changeRpcProvider;
 const explorer = chains.mainnet.taiko.explorer;
+const changeRpc = chains.mainnet.tempTaiko.changeRpcProvider;
 const tempProvider = chains.mainnet.tempTaiko.provider;
 const fs = require('fs');
 const moment = require('moment-timezone');
 const { displayHeader, delay } = require('./chains/utils/utils');
 const PRIVATE_KEYS = JSON.parse(fs.readFileSync('privateKeys.json', 'utf-8'));
-const { SEND_ABI } = require('./abi/abi');
+const { ABI, SEND_ABI } = require('./abi/abi');
+const WETH_CA = '0xA51894664A773981C6C112C43ce576f315d5b1B6';
 const SEND_CA = '0x2A5b0a407828b6Ca2E87e2e568CD8413fd5c24A1';
-const amountCheck = ethers.parseEther('1', 'ether');
-const gasPrice = ethers.parseUnits('0.2', 'gwei');
 const recipientsaddress = JSON.parse(fs.readFileSync('recipients.json', 'utf8'));
+const { CronJob } = require('cron');
+const amountCheck = ethers.parseEther('1', 'ether');
+const defaultgasPrice = ethers.parseUnits('0.19', 'gwei');
+async function getRoundedGasPrice(provider, defaultGasPrice) {
+  try {
+    let feeData = await provider.getFeeData();
+    let gasPrice = feeData.gasPrice;
+    
+    if (!gasPrice) throw new Error("Gas price not available");
+
+    let gasPriceRounded = ethers.parseUnits(
+      (Math.ceil(ethers.formatUnits(gasPrice, 'gwei') * 100) / 100).toString(),
+      'gwei'
+    );
+
+    console.log(`Gas price: ${ethers.formatUnits(gasPriceRounded, 'gwei')} gwei`.green);
+    return gasPriceRounded;
+  } catch (error) {
+    console.log(`Error: ${error.message}. Using default gas price ${ethers.formatUnits(defaultGasPrice, 'gwei')} gwei`);
+    return defaultGasPrice;
+  }
+}
 function appendLog(message) {
   fs.appendFileSync('log.txt', message + '\n');
 }
-const { CronJob } = require('cron');
 async function doSendEther(privateKey) {
   const wallet = new ethers.Wallet(privateKey, provider);
-  try {
-    const recipients = recipientsaddress;
-    const values = recipients.map(() => ethers.parseUnits('1.5', 'ether'));
-    const sendContract = new ethers.Contract(SEND_CA, SEND_ABI, wallet);
-    const txSendContract = await sendContract.multicall(recipients, values, { value: ethers.parseUnits('1.5', 'ether'), gasPrice: gasPrice });
-    const receipt = await txSendContract.wait(1);
-    return receipt.hash;
-  } catch (error) {
-    const errorMessage = `[$timezone] Error executing Send ETH transaction: ${error.message}`;
-    console.log(errorMessage.red);
-    appendLog(errorMessage);
+  const recipients = recipientsaddress;
+  const values = recipients.map(() => ethers.parseUnits('1.5', 'ether'));
+  const sendContract = new ethers.Contract(SEND_CA, SEND_ABI, wallet);
+  const maxRetries = 3;
+  let attempt = 0;
+
+  while (attempt < maxRetries) {
+    try {
+      const gasPrice = await getRoundedGasPrice(provider, defaultgasPrice);
+      const txSendContract = await sendContract.multicall(recipients, values, { value: ethers.parseUnits('1.5', 'ether'), gasPrice: gasPrice });
+      const receipt = await txSendContract.wait(1);
+      return receipt.hash;
+    } catch (error) {
+      attempt++;
+      const errorMessage = `[$timezone] Error executing Send ETH transaction (Attempt ${attempt}/${maxRetries}): ${error.message}`;
+      console.log(errorMessage.red);
+      appendLog(errorMessage);
+      if (attempt < maxRetries && (error.message.includes('insufficient funds') || 
+          error.message.includes('nonce has already') ||
+          error.message.includes('Gateway Timeout') ||
+          error.message.includes('failed to detect network') ||
+		  error.message.includes('request timeout') ||
+		  error.message.includes('free limit') ||
+          error.message.includes('missing revert data'))) {
+        console.log(`Retrying transaction after delay...`);
+        await delay(20000);
+        await changeProvider();
+      } else {
+        throw error;
+      }
+    }
   }
+
+  throw new Error(`Exceeded maximum retries for Send ETH transaction.`);
 }
 async function checkBalance(privateKey) {
   const wallet = new ethers.Wallet(privateKey, tempProvider);
